0072ac90bde389a523749e8b0530db6d9b7eeae7
pxaramillo <pxaramillo@yahoo.com>
2022-11-03 11:46:13 +0200

    Modified Footer CSS

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,9 +5,19 @@ import * as styles from './footer.module.css';
 const Footer = () => {
     return(
         <footer className={styles.footer}>
-            <div className="footerGrid">
-                <h2>Hello World</h2>
+            <div className={styles.footerGrid}>
+                <div>
+                    <h2>Company Information</h2>
+                </div>
+                <div>
+                    <h2>About Us</h2>
+                </div>
+                <div>
+                    <h2>Social Media</h2>
+                </div>
             </div>
+            <hr />
+            <p>Copyright © 2022. Ultek Solutions. Designed and Developed by Anvil Software Development. Powered by GatsbyJS and WordPress</p>
         </footer>
     )
 }
