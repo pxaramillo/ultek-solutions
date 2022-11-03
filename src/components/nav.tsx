3719599f4d0d35b6c666c29aa9adff468d08e8b3
pxaramillo <pxaramillo@yahoo.com>
2022-11-03 11:31:47 +0200

    Modified CSS

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -7,7 +7,7 @@ import * as styles from "./nav.module.css";
 
 const Nav = () => {
   return (
-    <div>
+    <div className={styles.navbar}>
       <button className={styles.btn}>
         <DarkModeIcon />
       </button>
