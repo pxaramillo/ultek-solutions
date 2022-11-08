db1f168df1464134acb8f51d4246fb3f8ff0ec8a
pxaramillo <pxaramillo@yahoo.com>
2022-11-09 00:22:21 +0200

    Added Framer Motion

--- cms-wordpress-app/pages/_app.tsx
+++ cms-wordpress-app/pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import Transition from '../components/transition'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Transition>
+      <Component {...pageProps} />
+    </Transition>
+  )
 }
 
 export default MyApp
