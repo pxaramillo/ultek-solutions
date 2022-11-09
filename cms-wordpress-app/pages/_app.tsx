26f4f656fafb39c74fc11a22b26bc1ab00edb9e9
Pedro <pxaramillo@yahoo.com>
2022-11-09 13:09:42 +0200

    Added React Bootstrap

--- cms-wordpress-app/pages/_app.tsx
+++ cms-wordpress-app/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Transition from '../components/transition'
 
 function MyApp({ Component, pageProps }: AppProps) {
