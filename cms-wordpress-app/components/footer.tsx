1742f27b1373bbc313aaa77348e12b5725fb8b2e
pxaramillo <pxaramillo@yahoo.com>
2022-11-14 13:53:53 +0200

    Social Media Icons

--- cms-wordpress-app/components/footer.tsx
+++ cms-wordpress-app/components/footer.tsx
@@ -1,5 +1,8 @@
-import Container from './container';
-import * as styles from './footer.module.css';
+import Container from "./container";
+import * as styles from "./footer.module.css";
+import { Facebook } from "react-bootstrap-icons";
+import Twitter from './icons/twitter.js';
+import Instagram from './icons/instagram';
 
 export default function Footer() {
   return (
@@ -8,7 +11,25 @@ export default function Footer() {
         <div>
           <h2>Company Information</h2>
         </div>
+        <hr />
+        <div className={styles.errata}>
+          <h6>
+            Copyright &copy; 2022, Ultek Solutions. Developed by Anvil Software
+            Development. Made with NextJS, WordPress and Woocommerce.
+          </h6>
+          <ul className={styles.social}>
+            <li>
+              <Facebook />
+            </li>
+            <li>
+              <Twitter />
+            </li>
+            <li>
+              <Instagram />
+            </li>
+          </ul>
+        </div>
       </Container>
     </footer>
-  )
+  );
 }
